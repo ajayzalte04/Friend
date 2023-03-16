f5d65833ae9bb9a02376d4a292c04f79046bcff8
ajayzalte04 <ajay@123>
2023-03-16 12:03:45 +0530

    hotstar commited

---
+++ src/jioPack/JioHotstar.java
@@ -0,0 +1,12 @@
+package jioPack;
+
+public class JioHotstar {
+
+	public static void main(String[] args) {
+
+		System.out.println("You can watch everything");
+
+		System.out.println("Sports movie tv webseries and more");
+	}
+
+}
