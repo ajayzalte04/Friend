1fa444492d847e63bb2208c3fe165a767f30bfda
ajayzalte04 <110119658+ajayzalte04@users.noreply.github.com>
2023-03-16 11:54:56 +0530

    Update JioTv.java
    Added some new feature...go through  it.. develop code for hotstar

--- src/jioPack/JioTv.java
+++ src/jioPack/JioTv.java
@@ -5,6 +5,9 @@ public class JioTv {
 	public static void main(String[] args) {
 		System.out.println("My jio app download");
 		System.out.println("My jio tv app download");
+		System.out.println("My jio movie app download");
+		System.out.println("My jio hotstar download");
+		System.out.println("My jio amazon download");
 	}
 
 }
